7e70f09ed522a88a7e6d081e343d0654b49c4e63
Kul Andya <atyanands@gmail.com>
2018-03-08 12:49:34 +0530

    FirstCommit

---
+++ app/src/main/java/com/example/app/BlueActivity.java
@@ -0,0 +1,245 @@
+package com.example.app;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+
+public class BlueActivity {
+    private static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+    private static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+    private static final int STATE_CONNECTED = 3;  // now connected to a remote device
+    private static final String TAG = "BluetoothChatService";
+    private static final boolean D = true;
+    private static final String NAME = "BluetoothChat";
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private final BluetoothAdapter mAdapter;
+    private final Handler mHandler;
+    byte[] buffer1 = {1, 2, 3, 4, 5};
+    int bytes1 = 1;
+    private ConnectThread mCT;
+    private ConnectedThread mCdT;
+    private SQLiteDatabase db;
+    private int mState;
+    private BluetoothSocket BTST;
+    private BluetoothDevice BTDV;
+    private MainActivity Main = null;
+
+    BlueActivity(Context context, Handler handler) {
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mState = STATE_NONE;
+        mHandler = handler;
+        Main = new MainActivity();
+
+
+    }
+
+    /*    public void mac_list(View v){
+            pairedDevices = BluetoothAdapter.getBondedDevices();
+            ArrayList list = new ArrayList();
+
+            for(BluetoothDevice bt : pairedDevices)
+                list.add(bt.getName());
+
+            final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+            lv1.setAdapter(adapter);
+        }
+    */
+    public synchronized int getState() {
+        return mState;
+    }
+
+    private synchronized void setState(int state) {
+        mState = state;
+    }
+
+
+    synchronized void connect(String address) {
+        if (mState == STATE_CONNECTING) {
+            if (mCT != null) {
+                mCT.cancel();
+                mCT = null;
+            }
+        }
+        if (mCdT != null) {
+            mCdT.cancel();
+            mCdT = null;
+        }
+        mCT = new ConnectThread(address);
+        mCT.start();
+        setState(STATE_CONNECTING);
+    }
+
+
+    private synchronized void connected() {
+        if (mState == STATE_CONNECTING) {
+            if (mCT != null) {
+                mCT.cancel();
+                mCT = null;
+            }
+        }
+        if (mCdT != null) {
+            mCdT.cancel();
+            mCdT = null;
+        }
+
+        mCdT = new ConnectedThread();
+        mCdT.start();
+
+        setState(STATE_CONNECTED);
+    }
+
+
+    synchronized void stop1() {
+        if (D) Log.d(TAG, "stop");
+        if (mCT != null) {
+            mCT.cancel();
+            mCT = null;
+        }
+        if (mCdT != null) {
+            mCdT.cancel();
+            mCdT = null;
+        }
+        setState(STATE_NONE);
+
+    }
+
+    private void write(String out) {
+
+        ConnectedThread r;
+        byte[] send = out.getBytes();
+        synchronized (this) {
+            if (mState != STATE_CONNECTED) return;
+            r = mCdT;
+        }
+        r.write(send);
+    }
+
+    void SwitchOn(int duty) {
+        String helsnd = "H" + duty + "X";
+        write(helsnd);
+    }
+
+    void SwitchOff() {
+        write("H000Y");
+    }
+
+    private void connectionFailed() {
+        mHandler.obtainMessage(5, 1, -1, buffer1).sendToTarget();
+    }
+
+    private void connectionLost() {
+        mHandler.obtainMessage(5, 1, -1, buffer1).sendToTarget();
+    }
+
+    private class ConnectThread extends Thread {
+        ConnectThread(String address) {
+            BluetoothDevice device = mAdapter.getRemoteDevice(address);
+            BTDV = device;
+            BluetoothSocket tmp = null;
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "create() failed", e);
+            }
+            BTST = tmp;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectThread");
+            setName("ConnectThread");
+
+            mAdapter.cancelDiscovery();
+            try {
+                BTST.connect();
+            } catch (IOException e) {
+                connectionFailed();
+
+                try {
+                    BTST.close();
+                } catch (IOException e2) {
+                    Log.e(TAG, "unable to close() socket during connection failure", e2);
+                }
+                return;
+            }
+            synchronized (BlueActivity.this) {
+                mCT = null;
+            }
+            connected();
+        }
+
+        void cancel() {
+            try {
+                BTST.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect socket failed", e);
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        ConnectedThread() {
+            Log.d(TAG, "create ConnectedThread");
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = BTST.getInputStream();
+                tmpOut = BTST.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "temp sockets not created", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectedThread");
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);
+                    mHandler.obtainMessage(MainActivity.BLUE_ONE, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    Log.e(TAG, "disconnected", e);
+                    connectionLost();
+                    break;
+                }
+            }
+
+        }
+
+        void write(byte[] buffer) {
+            try {
+                mmOutStream.write(buffer);
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during write", e);
+            }
+        }
+
+        void cancel() {
+            try {
+                BTST.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect socket failed", e);
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,252 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    public static final int BLUE_ONE = 1;
+    public static final String PREFERENCES = "preferences";
+    public static final String MAC_ID = "mac_id";
+    public static final String SPLIT = "____";
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final Handler mHandler = new Handler() {
+
+
+        @Override
+        public void handleMessage(Message msg) {
+        }
+
+    };
+
+    String[] blue_list = null;
+    private BluetoothAdapter btAdapter;
+    private BlueActivity mBlueActivity = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        mBlueActivity = new BlueActivity(this, mHandler);
+
+
+        Switch switch_1 = findViewById(R.id.switch_1);
+        Switch switch_2 = findViewById(R.id.switch_2);
+        Switch switch_3 = findViewById(R.id.switch_3);
+        Switch switch_4 = findViewById(R.id.switch_4);
+        Switch switch_5 = findViewById(R.id.switch_5);
+        Switch switch_6 = findViewById(R.id.switch_6);
+        Switch switch_7 = findViewById(R.id.switch_7);
+        Switch switch_8 = findViewById(R.id.switch_8);
+
+        switch_1.setOnCheckedChangeListener(this);
+        switch_2.setOnCheckedChangeListener(this);
+        switch_3.setOnCheckedChangeListener(this);
+        switch_4.setOnCheckedChangeListener(this);
+        switch_5.setOnCheckedChangeListener(this);
+        switch_6.setOnCheckedChangeListener(this);
+        switch_7.setOnCheckedChangeListener(this);
+        switch_8.setOnCheckedChangeListener(this);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        CheckBTState();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    public void Connect_bluetooth(View view) {
+        final SharedPreferences settings = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+
+        if (settings.contains(MAC_ID)) {
+            String address1 = settings.getString(MAC_ID, null);
+            boolean on = ((ToggleButton) view).isChecked();
+            if (on) {
+                mBlueActivity.connect(address1);
+                Toast.makeText(this, "Connecting to " + address1, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Disconnecting from " + address1, Toast.LENGTH_LONG).show();
+                mBlueActivity.stop1();
+            }
+        } else {
+            set_mac_id(new View(this));
+
+        }
+
+    }
+
+    public void set_mac_id(View view) {
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        ArrayList<String> names = new ArrayList<>();
+        for (BluetoothDevice bt : pairedDevices) {
+            names.add(bt.getName() + SPLIT + bt.getAddress());
+        }
+        blue_list = new String[names.size()];
+        blue_list = names.toArray(blue_list);
+        mac_list();
+    }
+
+    public void mac_list() {
+        final SharedPreferences settings = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+
+        new AlertDialog.Builder(this)
+                .setTitle("yoo")
+                .setCancelable(true)
+                .setItems(blue_list, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String[] address = blue_list[which].split(SPLIT);
+                        settings.edit().putString(MAC_ID, address[1]).apply();
+                        Toast.makeText(MainActivity.this, "Saving this " + address[0] + " Device at address " + address[1], Toast.LENGTH_LONG).show();
+
+                    }
+                })
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        //   finish();
+
+
+                    }
+                })
+                .show();
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBlueActivity.stop1();
+    }
+
+    private void CheckBTState() {
+        if (btAdapter == null) {
+            AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
+        } else {
+            if (!btAdapter.isEnabled()) {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    public void AlertBox(final String title, String message) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message + " Press OK to exit.")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        //   finish();
+                        title.length();
+
+                    }
+                }).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        switch (buttonView.getId()) {
+            case R.id.switch_1:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(1);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+            case R.id.switch_2:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(2);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+            case R.id.switch_3:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(3);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+            case R.id.switch_4:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(4);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+            case R.id.switch_5:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(5);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+            case R.id.switch_6:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(6);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+            case R.id.switch_7:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(7);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+            case R.id.switch_8:
+                if (isChecked) {
+                    mBlueActivity.SwitchOn(8);
+                } else {
+                    mBlueActivity.SwitchOff();
+                }
+                break;
+        }
+    }
+}
